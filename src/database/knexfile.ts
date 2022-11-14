1392f9acf505f659197df4538ca3cd807efeb54a
oluwadamoo <mrdamilolasaliu@gmail.com>
2022-11-14 21:57:22 +0100

    update knexfile for production env

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -24,6 +24,7 @@ const config: { [key: string]: Knex.Config } = {
   production: {
     client: "mysql2",
     connection: {
+      host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD
